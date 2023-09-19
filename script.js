062e2a122d99a95c01e06caa8168eddf1c985c12
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-19 13:14:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const para= document.getElementById("status");
+const button = document.getElementById("enterBtn");
+button.addEventListener("click",() =>{
+	para.innerHTML =`<h1> Entered Metaverse </h1>`;
+})
